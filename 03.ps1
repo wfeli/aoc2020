f952144738ebce1b145eed3e3ca3325dacfe1beb
wfeli <laura.werfeli@hotmail.ch>
2020-12-04 21:55:59 +0100

    cleaner

--- 03.ps1
+++ 03.ps1
@@ -1,21 +1,4 @@
 $con = Get-Content .\input\03.txt
-#$con = Get-Content .\input\03test.txt
-$right = 0
-$treecount = 0
-foreach ($row in $con) {
-    if ($row.ToCharArray()[$right] -eq "#") {
-        $treecount++
-    }
-    if (($right + 3) -gt 30) {
-        $right = ($right + 3) - 31
-        continue
-    }
-    $right = $right + 3
-}
-Write-Host $treecount
-
-## Part 2
-
 function Get-TobogganPath {
     param (
         $D,
@@ -31,8 +14,8 @@ function Get-TobogganPath {
         if ($array[$i].ToCharArray()[$right] -eq "#") {
             $treecount++
         }
-        if (($right + $r) -gt 30) {
-            $right = ($right + $r) - 31
+        if (($right + $r) -gt ($array[$i].length -1)) {
+            $right = ($right + $r) - $array[$i].length
         }
         else {
             $right = $right + $r
@@ -40,4 +23,6 @@ function Get-TobogganPath {
     }
     return $treecount
 }
+Get-TobogganPath -array $con -D 1 -R 3
+## Part 2
 (Get-TobogganPath -array $con -D 1 -R 1) * (Get-TobogganPath -array $con -D 1 -R 3) * (Get-TobogganPath -array $con -D 1 -R 5) * (Get-TobogganPath -array $con -D 1 -R 7) * (Get-TobogganPath -array $con -D 2 -R 1)
